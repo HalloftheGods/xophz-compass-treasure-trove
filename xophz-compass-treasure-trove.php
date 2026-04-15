64ae83d02782e89456313161feaa485ffc3cef29
Xopher <xopher@vi.co>
2026-04-14 21:30:44 -0700

    chore: sync plugins

--- xophz-compass-treasure-trove.php
+++ xophz-compass-treasure-trove.php
@@ -19,7 +19,7 @@
  * Description:       The Sovereign Data Vault: Securely encrypt, store, and manage your personal data and privacy settings.
  * Version:           26.4.14.1312
  * Author:            Hall of the Gods, Inc.
- * Author URI:        http://www.midnightnerd.com/
+ * Author URI:        http://www.hallofthegods.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       xophz-compass-treasure-trove
